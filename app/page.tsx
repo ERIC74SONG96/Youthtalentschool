b7fe5c7373a702f6577250cab7035bfbb428105a
ERIC74SONG96 <tchuisseugedeon@gmail.com>
2026-05-06 14:44:52 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -276,7 +276,7 @@ export default function HomePage() {
                 <div className="w-11 h-11 rounded-full bg-[#f2b705] flex items-center justify-center">
                   <MapPin className="w-5 h-5 text-[#0b1c2d]" />
                 </div>
-                <span className="text-lg">Boulevard de la Camre 48, 1000 Bruxelles</span>
+                <span className="text-lg">Boulevard de la Cambre 48, 1000 Bruxelles</span>
               </div>
             </div>
 
